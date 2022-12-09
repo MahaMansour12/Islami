457507536ad39d8688f184732e0a31f75be2130d
MahaMansour <mahamansour12345@gamil.com>
2022-12-09 14:07:58 -0800

    add sura details add sura name

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:islamy/home_screen.dart';
+import 'package:islamy/My%20Them.dart';
+import 'package:islamy/home/Quran/SuraDetails.dart';
+import 'package:islamy/home/home_screen.dart';
 
 void main() {
   runApp(Islami());
@@ -13,7 +15,10 @@ class Islami extends StatelessWidget {
       initialRoute: Home_Screen.routNam,
       routes: {
         Home_Screen.routNam: (context) => Home_Screen(),
+        Sura_Name_Details.routName: (context) => Sura_Name_Details(),
       },
+      theme: MyThem.lightThem,
+      darkTheme: MyThem.darktThem,
     );
   }
 }
